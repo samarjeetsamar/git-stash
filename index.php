7b9e4ad9d0b95d9a710df3355f4b8843c44097b7
samarjeet kumar <kumarsamarjeet1@gmail.com>
2023-11-18 16:14:25 +0530

    worked on index.php

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
 <?php 
 
-echo "feature 1";
-echo "some changes by me";
-echo "some changes by other developer";
-echo "feature 2";
-
-
-echo "some changes";
-
-echo "some feature added";
-echo "more changes in feature";
